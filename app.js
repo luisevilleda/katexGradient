3113bccd6b5add061cb59232567c1fdcfe02f46c
Luis Villeda <lvilleda@fb.com>
2018-07-17 16:28:34 -0700

    fix horizontal gradient

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const WordWrap = require('./node_modules/word-wrap/index.js');
 const horizontalGradient = function(string, color1, color2) {
     const splitText = string.split('');
     const gradient = JsGradient.generateGradient(color1, color2, splitText.length);
-    const prefix = '\\(\\stringsf{{';
+    const prefix = '\\(\\textsf{{';
     const suffix = '}}\\)';
     const body = splitText.map((letter, index) => `\\color{${gradient[index]}}${letter}`);
     return prefix + body.join('') + suffix;
@@ -28,16 +28,6 @@ const verticalGradient = function(string, color1, color2, characterLimit = 40) {
 };
 
 // console.log(horizontalGradient('i lkajsf laksjdf alskdfj lasdkjf lasdkjf alskdjf alskdjf alksdjf lkasdj am angery', 'E83535', '35D3E8'));
-const copypasta = [ 'What the fuck did you just fucking say ',
-    'about me, you little bitch? I\'ll have ',
-    'you know I graduated top of my class in ',
-    'the Navy Seals, and I\'ve been involved ',
-    'in numerous secret raids on Al-Quaeda, ',
-    'and I have over 300 confirmed kills. I ',
-    'am trained in gorilla warfare and I\'m ',
-    'the top sniper in the entire US armed ',
-    'forces. You are nothing to me but just ',
-    'another target. I will wipe you the fuck ',
-    'out with precision the likes of which ',
-    'has never been seen before on this'].join('');
-console.log(verticalGradient(copypasta, 'ee4c5b', '#FFD460'));
+// const copypasta = `What the fuck did you just fucking say about me, you little shit? I’ll have you know I graduated top of my contingent orientation 4 hours class, and I’ve been getting redefines expectations and not even having to get performance reviews, and I have caused over 300 confirmed SEV-1s. I was trained in the art of HACK and I’m the top contingent worker in the entire Facebook Engineering Org. You are nothing to me but just another FBID. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me on Facebook? Think again, fucker. As we speak I am contacting my secret network of Contingent Workers across FB and your FBID is being checkpointed right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can TROLL you in over seven hundred ways, and that’s just with crushinator. Not only am I extensively trained in Javascript and HTML, but I have access to the entire arsenal of WWW and I will use it to its full extent to wipe your miserable ass off the face of the Internet, you little shit.`;
+const copypasta = 'no u';
+console.log(horizontalGradient(copypasta, '#ee4c5b', '#FFD460'));
